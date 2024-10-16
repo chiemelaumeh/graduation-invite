f1ea1c1af0a52e41533f5f3483cb4927d23b2f0f
chiemelaumeh <chiemelaumeh1@gmail.com>
2024-10-16 10:09:13 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import CountdownTimer from "./countDown";
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CountdownTimer />
+
     </div>
+    <div className="App"> Only Nana gotta pay a $500 entry fee 🤷🏾‍♂️ </div>
+    
+    
+    </>
   );
 }
 
